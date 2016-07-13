c6b84e9e4e7941b8a00ebb58b89fe1507390a4ff
hellozts4120 <14307110190@fudan.edu.cn>
2016-07-13 21:11:59 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import mkdirp from 'mkdirp';
+import path from 'path';
+import {createRandomFile, isURL} from './utils';
+import downloadFile from './download';
+import copyFile from './copy';
+
+// qDown(source, callback);
+// qDown(source, target, callback)
+export default function qDown(...args) {
+	var source, callback;
+	if (args.length < 2) {
+		throw new Error('invalid argument number!');
+	}
+
+	source = args[0];
+	if (args.length === 2) {
+		callback = args[1];
+		target = createRandomFile(qDown.tmpDir);
+	} else {
+		target = args[1];
+		callback = args[2];
+	}
+
+	mkdirp(path.dirname(target), err => {
+		if (err) {
+			return callback(err);
+		} else {
+			if (isURL(source)) {
+				// todo
+				downloadFile(source, target, callback);
+			} else {
+				// todo
+				copyFile(source, target, callback);
+			}
+		}
+	});
+}
---
+++ src/utils.js
@@ -0,0 +1,34 @@
+import os from 'os';
+import path from 'path';
+
+let getTmpDir = os.tmpdir || os.tmpDir;
+
+export function callback(fn) {
+	let hasCallback = false;
+	return function(...args) {
+		if (hasCallback) {
+			console.log("callback twice!");
+		} else {
+			hasCallback = true;
+			console.log("callback!");
+			fn(...args);
+		}
+	}
+}
+
+export function createRandomString(size = 6, str = 'abcdefghijklmnopqrstuvwxyz0123456789') {
+	let res = '';
+	while(size) {
+		res += str.charAt(Math.floor(Math.random() * (str.length + 1)));
+	}
+
+	return res;
+}
+
+export function createRandomFile(tmpDir = getTmpDir()) {
+	return path.resolve(tmpDir, createRandomString(20));
+}
+
+export function isURL(url) {
+	return (url.substr(0, 7) === 'http://') || (url.substr(0, 8) === 'https://');
+}
