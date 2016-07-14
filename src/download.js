28eb3cbb0b41654d9f7e31164a09ad7c63b009af
hellozts4120 <14307110190@fudan.edu.cn>
2016-07-14 20:43:43 +0800

    implement download module

--- src/download.js
+++ src/download.js
@@ -1,2 +1,35 @@
 import * as utils from './utils';
 import fs from 'fs';
+import request from 'request';
+
+export function downloadFile(url, target, callback) {
+	callback = utils.callback(callback);
+
+	let stream = fs.createWriteStream(target);
+	stream.on('error', err => {
+		console.log('open target file with error: ' + err);
+		callback(err);
+	});
+
+	let totalSize = 0;
+	let downloadSize = 0;
+	let req = request
+		.get({
+			url: url,
+			encoding: null
+		})
+		.on('response', res => {
+			if (res.statusCode !== 200) {
+				return callback(new Error('status #' + res.statusCode));
+			}
+			totalSize = res.headers['content-length'] || null;
+
+			res.on('data', data => {
+				downloadSize += data.length;
+				console.log('download file progress: ' + downloadSize + ' / ' + totalSize);			
+			});
+			res.on('end', _ => callback(null, target));
+		})
+		.pipe(stream);
+
+}
