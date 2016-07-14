40ab86203e948877fd372c57afca9a0374790c69
hellozts4120 <14307110190@fudan.edu.cn>
2016-07-14 20:49:24 +0800

    modify indent

--- src/copy.js
+++ src/copy.js
@@ -3,31 +3,31 @@ import path from 'path';
 import fs from 'fs';
 
 export default function copyFile(source, target, callback) {
-	callback = utils.callback(callback);
+    callback = utils.callback(callback);
 
-	fs.stat(source, (err, stats) => {
-		if (err) return callback(err);
+    fs.stat(source, (err, stats) => {
+        if (err) return callback(err);
 
-		let sourceStream = fs.createReadStream(source),
-			targetStream = fs.createWriteStream(target);
+        let sourceStream = fs.createReadStream(source),
+            targetStream = fs.createWriteStream(target);
 
-		sourceStream.on('error', err => {
-			console.log('Error when open source file: ' + err);
-			callback(err);
-		});
-		targetStream.on('error', err => {
-			console.log('Error when open target file: ' + err);
-			callback(err);
-		});
+        sourceStream.on('error', err => {
+            console.log('Error when open source file: ' + err);
+            callback(err);
+        });
+        targetStream.on('error', err => {
+            console.log('Error when open target file: ' + err);
+            callback(err);
+        });
 
-		let copySize = 0;
-		sourceStream.on('data', data => {
-			copySize += data.length;
-			console.log('copy file progress: ' + copySize + ' / ' + stats.size);
-		});
+        let copySize = 0;
+        sourceStream.on('data', data => {
+            copySize += data.length;
+            console.log('copy file progress: ' + copySize + ' / ' + stats.size);
+        });
 
-		sourceStream.on('end', _ => callback(null, target));
+        sourceStream.on('end', _ => callback(null, target));
 
-		sourceStream.pipe(targetStream);
-	})
+        sourceStream.pipe(targetStream);
+    })
 }
--- src/utils.js
+++ src/utils.js
@@ -4,31 +4,31 @@ import path from 'path';
 let getTmpDir = os.tmpdir || os.tmpDir;
 
 export function callback(fn) {
-	let hasCallback = false;
-	return function(...args) {
-		if (hasCallback) {
-			console.log("callback again!");
-		} else {
-			hasCallback = true;
-			console.log("callback!");
-			fn(...args);
-		}
-	}
+    let hasCallback = false;
+    return function(...args) {
+        if (hasCallback) {
+            console.log("callback again!");
+        } else {
+            hasCallback = true;
+            console.log("callback!");
+            fn(...args);
+        }
+    }
 }
 
 export function createRandomString(size = 6, str = 'abcdefghijklmnopqrstuvwxyz0123456789') {
-	let res = '';
-	while(size) {
-		res += str.charAt(Math.floor(Math.random() * (str.length + 1)));
-	}
+    let res = '';
+    while(size) {
+        res += str.charAt(Math.floor(Math.random() * (str.length + 1)));
+    }
 
-	return res;
+    return res;
 }
 
 export function createRandomFile(tmpDir = getTmpDir()) {
-	return path.resolve(tmpDir, createRandomString(20));
+    return path.resolve(tmpDir, createRandomString(20));
 }
 
 export function isURL(url) {
-	return (url.substr(0, 7) === 'http://') || (url.substr(0, 8) === 'https://');
+    return (url.substr(0, 7) === 'http://') || (url.substr(0, 8) === 'https://');
 }
